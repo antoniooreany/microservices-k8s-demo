da4c1df8a0230e8555aec7788f4749cce7ab96d3
antoniooreany <4531858+antoniooreany@users.noreply.github.com>
2025-05-23 15:26:08 +0200

    Add initial microservices setup with frontend and backend components.
    Works! Need to be unhardcoded PROJECT_ID in backend-deployment.yaml, frontend-deployment.yaml

---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('http://backend-service:8080/api/message');
+        res.send(`<h1>${response.data.message}</h1>`);
+    } catch (error) {
+        res.status(500).send('Error connecting to backend');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Frontend running on port 3000');
+});
