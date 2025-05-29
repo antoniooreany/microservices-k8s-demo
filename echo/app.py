f200f0a417f9866a22678dbf444bb54f60ee1d07
antoniooreany <4531858+antoniooreany@users.noreply.github.com>
2025-05-29 14:27:48 +0200

    Add secret key endpoint and update ingress configuration for echo service

--- echo/app.py
+++ echo/app.py
@@ -1,9 +1,16 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
     return "pong"
+
+@app.route("/secret-key")
+def secret_key():
+    secret = os.getenv("SECRET_KEY", "SECRET_KEY not found")
+    return f"SECRET_KEY: {secret}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
